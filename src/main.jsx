ce692d3f01d55e2ed343233a70b924d8b45dfafd
shengkai pianggong <pianggong@gmail.com>
2026-05-24 12:37:05 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import PhraseApp from "../components/PhraseApp";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PhraseApp from '../components/PhraseApp'
 
-function App() {
-  return <PhraseApp />;
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PhraseApp />
+  </React.StrictMode>
+)
 
-export default App;
